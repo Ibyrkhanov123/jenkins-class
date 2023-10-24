619d420ed895868fc826f049f7d06c7315c57d31
Arman Ibyrkhanov <Ibyrkhanov.123@gmail.com>
2023-10-23 19:07:02 -0500

    created stage 2

--- Jenkinsfile_var.groovy
+++ Jenkinsfile_var.groovy
@@ -2,11 +2,18 @@ pipeline {
     agent any
     environment {
         Var1 = 'Value1'
+        Var2 = 'Value2'
+
     }
     stages {
         stage('build') {
             steps {
                 echo "Var1 value is ${Var1}"
+            }
+        }
+        stage('test') {
+            steps {
+                echo "Var2 value is ${Var2}"
             }
         }
     }
