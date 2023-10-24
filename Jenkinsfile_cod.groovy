c4353c63bef7494e8198d363215aab3abfa5e7ef
Arman Ibyrkhanov <Ibyrkhanov.123@gmail.com>
2023-10-23 20:44:06 -0500

    fixed code

--- Jenkinsfile_cod.groovy
+++ Jenkinsfile_cod.groovy
@@ -6,12 +6,12 @@ pipeline {
     stages {
         stage('stage1') {
             steps {
-                echo "${params.git_branch}"
+                echo "${params.branch}"
             }
         }
         stage('stage2') {
             steps {
-                echo "${params.git_branch}"
+                echo "${params.branch}"
             }
         }
     }
