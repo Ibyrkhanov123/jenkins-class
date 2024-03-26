bbbfecff6c0b863bed16bba333125f3140d7953a
Arman Ibyrkhanov <Ibyrkhanov.123@gmail.com>
2024-03-26 18:02:31 -0500

    added some lines

--- new/environment.groovy
+++ new/environment.groovy
@@ -3,12 +3,18 @@ pipeline{
 
     environment {
         VAR1 = "building"
+        VAR2 = "testing"
     }
 
     stages {
         stage('Build') {
             steps {
                 echo "VAR1 value is ${VAR1}"
+            }
+        }
+        stage('test') {
+            steps {
+                echo "VAR2 value is ${VAR2}"
             }
         }
     }
