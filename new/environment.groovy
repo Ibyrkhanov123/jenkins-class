6b0e73f3796b4ddb5f3fe0e085f1966650599550
Arman Ibyrkhanov <Ibyrkhanov.123@gmail.com>
2024-03-26 18:05:28 -0500

    added some lines

--- new/environment.groovy
+++ new/environment.groovy
@@ -4,12 +4,14 @@ pipeline{
     environment {
         VAR1 = "building"
         VAR2 = "testing"
+        AWS_ACCESS_KEY = credentials('aws-access-key')
     }
 
     stages {
         stage('Build') {
             steps {
                 echo "VAR1 value is ${VAR1}"
+                echo "AWS access key is ${AWS_ACCESS_KEY}"
             }
         }
         stage('test') {
