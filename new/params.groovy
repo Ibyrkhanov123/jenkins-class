3f5d06ceb9fd186fca52da0928468d305e76a763
Arman Ibyrkhanov <Ibyrkhanov.123@gmail.com>
2024-03-26 19:49:49 -0500

    added some lines

--- new/params.groovy
+++ new/params.groovy
@@ -5,8 +5,10 @@ pipeline{
     }
 
     stages {
-        steps {
-            echo "$params.USER_NAME is running the pipeline"
+        stage('stage1'){
+            steps {
+                echo "$params.USER_NAME is running the pipeline"
+            }
         }
     }
 }
