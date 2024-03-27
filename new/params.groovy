960211e578e0c47d11197199d781dba8a0b54163
Arman Ibyrkhanov <Ibyrkhanov.123@gmail.com>
2024-03-26 19:58:51 -0500

    added some lines

--- new/params.groovy
+++ new/params.groovy
@@ -1,13 +1,14 @@
 pipeline{
     agent any
     parameters {
-        string(name: 'USER_NAME', description: 'Who is running the pipeline?' , defaultValue: 'admin')
+        string(name: 'USER_NAME', description: 'Who is running the pipeline?' , defaultValue: 'admin', trim: true)
     }
 
     stages {
         stage('stage1'){
             steps {
                 echo "$params.USER_NAME is running the pipeline"
+                echo "${params.USER_NAME} is running the pipeline"
             }
         }
     }
