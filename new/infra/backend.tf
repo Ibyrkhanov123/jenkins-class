389c3a45a1518a77a37119a78d4c997a037962a5
Arman Ibyrkhanov <Ibyrkhanov.123@gmail.com>
2024-03-28 17:15:38 -0500

    created new file

--- new/infra/backend.tf
+++ new/infra/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "terraform-class-backend-arman-bucket"
-    key    = "jenkins-class/app-infra/terraform.tfstate"
+    key    = "jenkins-class/new/app-infra/terraform.tfstate"
     region = "us-east-1"
   }
 }
